20643d4d74d00854d7c834a5d555651f2797567c
MeganeRenault <mawafmegane@gmail.com>
2024-05-12 12:11:17 +0200

    Committing Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         int a = 2, b = 2;
-        int sum = a + b;
-        System.out.println(a + " + " + b + " = " + sum);
+        int som = a + b;
+        System.out.println(a + " + " + b + " = " + som);
     }
 }
